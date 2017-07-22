0caa4845cd3f498cd8f7453b01f0f9b763a5f0c1
Michael Pope <mpope.cr@gmail.com>
2017-07-22 09:09:54 -0700

    Create initial commit

---
+++ lib/claws.ex
@@ -0,0 +1,18 @@
+defmodule CLAWS do
+  @moduledoc """
+  Documentation for CLAWS.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CLAWS.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/claws_test.exs
@@ -0,0 +1,8 @@
+defmodule CLAWSTest do
+  use ExUnit.Case
+  doctest CLAWS
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
